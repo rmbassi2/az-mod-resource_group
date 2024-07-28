bf98c9a3cc826331716138dcb51ac1f5c411f16b
Rob <bassir@michigan.gov>
2024-07-28 15:00:23 -0400

    changed default var again

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "Testing area"
+  default     = "Testing area1"
 }
 
 variable "location" {
